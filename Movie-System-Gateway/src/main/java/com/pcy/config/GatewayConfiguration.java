287eabbbf4c66234b7ca148b3c321d5ea482cbb6
pengchenyu111 <1106998960@qq.com>
2023-03-11 17:30:50 +0800

    添加网关

---
+++ Movie-System-Gateway/src/main/java/com/pcy/config/GatewayConfiguration.java
@@ -0,0 +1,97 @@
+package com.pcy.config;
+
+import com.alibaba.csp.sentinel.adapter.gateway.common.SentinelGatewayConstants;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiDefinition;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiPathPredicateItem;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiPredicateItem;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.GatewayApiDefinitionManager;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.BlockRequestHandler;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.exception.SentinelGatewayBlockExceptionHandler;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.result.view.ViewResolver;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.PostConstruct;
+import java.util.*;
+
+/**
+ * Created by lanxw
+ * 限流处理
+ */
+@Configuration
+public class GatewayConfiguration {
+    private final List<ViewResolver> viewResolvers;
+    private final ServerCodecConfigurer serverCodecConfigurer;
+    public GatewayConfiguration(ObjectProvider<List<ViewResolver>> viewResolversProvider,
+                                ServerCodecConfigurer serverCodecConfigurer) {
+        this.viewResolvers = viewResolversProvider.getIfAvailable(Collections::emptyList);
+        this.serverCodecConfigurer = serverCodecConfigurer;
+    }
+    // 配置限流的异常处理器
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SentinelGatewayBlockExceptionHandler sentinelGatewayBlockExceptionHandler() {
+        // Register the block exception handler for Spring Cloud Gateway.
+        return new SentinelGatewayBlockExceptionHandler(viewResolvers, serverCodecConfigurer);
+    }
+    // 初始化一个限流的过滤器
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public GlobalFilter sentinelGatewayFilter() {
+        return new SentinelGatewayFilter();
+    }
+    @PostConstruct
+    public void initBlockHandlers() {
+        BlockRequestHandler blockRequestHandler = new BlockRequestHandler() {
+            public Mono<ServerResponse> handleRequest(ServerWebExchange serverWebExchange, Throwable throwable) {
+                Map map = new HashMap<>();
+                map.put("code", 0);
+                map.put("message", "接口被限流了");
+                return ServerResponse.status(HttpStatus.OK).
+                        contentType(MediaType.APPLICATION_JSON).
+                        body(BodyInserters.fromValue(map));
+            }
+        };
+        GatewayCallbackManager.setBlockHandler(blockRequestHandler);
+    }
+    @PostConstruct
+    private void initCustomizedApis() {
+        Set<ApiDefinition> definitions = new HashSet<>();
+        ApiDefinition api1 = new ApiDefinition("order_api1")
+                .setPredicateItems(new HashSet<ApiPredicateItem>() {{
+                    add(new ApiPathPredicateItem().setPattern("/order-serv/api1/**").
+                            setMatchStrategy(SentinelGatewayConstants.URL_MATCH_STRATEGY_PREFIX));
+                }});
+        definitions.add(api1);
+        GatewayApiDefinitionManager.loadApiDefinitions(definitions);
+    }
+    //增加对商品微服务的 限流
+    @PostConstruct
+    private void initGatewayRules() {
+        Set<GatewayFlowRule> rules = new HashSet<>();
+       /* rules.add(new GatewayFlowRule("product_route")
+                .setCount(3)
+                .setIntervalSec(1)
+        );
+        rules.add(new GatewayFlowRule("order_api1").
+                setCount(1).
+                setIntervalSec(1));*/
+        GatewayRuleManager.loadRules(rules);
+    }
+
+}
---
+++ Movie-System-Gateway/src/main/java/com/pcy/filters/CommonFilter.java
@@ -0,0 +1,55 @@
+package com.pcy.filters;
+
+import com.pcy.commmon.constant.CommonConstants;
+import com.pcy.commmon.redis.CommonRedisKey;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+/**
+ * Created by lanxw
+ * 定义全局过滤器，功能如下:
+ * 1.把客户端真实IP通过请求同的方式传递给微服务
+ * 2.在请求头中添加FEIGN_REQUEST的请求头，值为0，标记请求不是Feign调用，而是客户端调用
+ * 3.刷新Token的有效时间
+ */
+@Component
+public class CommonFilter implements GlobalFilter {
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        /**
+         * pre拦截逻辑
+         * 在请求去到微服务之前，做了两个处理
+         * 1.把客户端真实IP通过请求同的方式传递给微服务
+         * 2.在请求头中添加FEIGN_REQUEST的请求头，值为0，标记请求不是Feign调用，而是客户端调用
+         */
+        ServerHttpRequest request = exchange.getRequest().mutate().
+                header(CommonConstants.REAL_IP, Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getHostString()).
+                header(CommonConstants.FEIGN_REQUEST_KEY, CommonConstants.FEIGN_REQUEST_FALSE).
+                build();
+        return chain.filter(exchange.mutate().request(request).build()).then(Mono.fromRunnable(() -> {
+            /**
+             * post拦截逻辑
+             * 在请求执行完微服务之后,需要刷新token在redis的时间
+             * 判断token不为空 && Redis还存在这个token对于的key,这时候需要延长Redis中对应key的有效时间.
+             */
+            String token, redisKey;
+            if (!StringUtils.isEmpty(token = exchange.getRequest().getHeaders().getFirst(CommonConstants.TOKEN_NAME))
+                    && redisTemplate.hasKey(redisKey = CommonRedisKey.USER_TOKEN.getRealKey(token))) {
+                redisTemplate.expire(redisKey, CommonRedisKey.USER_TOKEN.getExpireTime(), CommonRedisKey.USER_TOKEN.getUnit());
+            }
+        }));
+    }
+}
