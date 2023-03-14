5d6c4b4d118c9eb24b74cd6f29d094797f4efe4a
pengchenyu111 <1106998960@qq.com>
2023-03-14 20:36:32 +0800

    重构推荐服务

---
+++ Movie-System-Recommend/Movie-System-Recommend-Service/src/main/java/com/pcy/service/RecommendService.java
@@ -0,0 +1,96 @@
+package com.pcy.service;
+
+
+import com.pcy.domain.movieDetail.MovieDetail;
+
+import java.util.List;
+
+/**
+ * 推荐服务
+ *
+ * @author PengChenyu
+ * @since 2021-02-02 15:12:57
+ */
+public interface RecommendService {
+
+    /**
+     * 历史热门Top20电影
+     *
+     * @return 对象列表
+     */
+    List<MovieDetail> historyTop20();
+
+
+    /**
+     * 近期热门Top20电影
+     *
+     * @return 对象列表
+     */
+    List<MovieDetail> recentlyTop20();
+
+
+    /**
+     * 分类Top10电影
+     *
+     * @param genre 分类名
+     * @return 对象列表
+     */
+    List<MovieDetail> genreTop10(String genre);
+
+
+    /**
+     * 多分类综合Top10
+     *
+     * @param genreList 分类列表
+     * @return 对象列表
+     */
+    List<MovieDetail> genreCompositeTop10(List<String> genreList);
+
+    /**
+     * 用户感兴趣标签推荐Top10
+     *
+     * @param userId 用户id
+     * @return 对象列表
+     */
+    List<MovieDetail> userPreferGenreTop10(Integer userId);
+
+    /**
+     * 基于内容的TF-IDF电影推荐
+     *
+     * @param doubanId 豆瓣id
+     * @return 对象列表
+     */
+    List<MovieDetail> contentTFIDF(Integer doubanId);
+
+    /**
+     * 基于ALS的用户电影推荐
+     *
+     * @param userId 用户id
+     * @return 推荐列表
+     */
+    List<MovieDetail> alsUserRecs(Integer userId);
+
+    /**
+     * 基于ALS的电影相似度推荐
+     *
+     * @param doubanId 豆瓣id
+     * @return 推荐列表
+     */
+    List<MovieDetail> alsMovieSimRecs(Integer doubanId);
+
+    /**
+     * 基于ItemCF的电影相似度推荐
+     *
+     * @param doubanId 豆瓣id
+     * @return 推荐列表
+     */
+    List<MovieDetail> itemCFRecs(Integer doubanId);
+
+    /**
+     * 基于实时评分的电影推荐
+     *
+     * @param userId 用户id
+     * @return 推荐列表
+     */
+    List<MovieDetail> ratingRecs(Integer userId);
+}
