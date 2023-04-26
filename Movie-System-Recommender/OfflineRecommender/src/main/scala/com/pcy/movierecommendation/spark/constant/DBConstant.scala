ad8f9a13ce3784773f00340da924a82d29dc58dc
pengchenyu111 <1106998960@qq.com>
2023-04-26 19:39:43 +0800

    添加离线推荐与实时推荐模块

---
+++ Movie-System-Recommender/OfflineRecommender/src/main/scala/com/pcy/movierecommendation/spark/constant/DBConstant.scala
@@ -0,0 +1,78 @@
+package com.pcy.movierecommendation.spark.constant
+
+/**
+ * 数据库常量
+ *
+ * @author PengChenyu
+ * @since 2020-12-18 17:42:00
+ */
+object
+DBConstant {
+
+  /**
+   * MongoDB配置
+   */
+  val MONGO_URL = "mongodb://designer:000000@192.168.10.181:27017/recommender";
+  val MONGO_DB = "recommender";
+
+  /**
+   * 历史热门电影TOP20表
+   */
+  val MONGO_COLLECTION_HISTORY_TOP_20 = "history_top_20";
+
+  /**
+   * 近期热门电影TOP表
+   */
+  val MONGO_COLLECTION_RECENTLY_TOP = "recently_top";
+
+  /**
+   * 近期热门电影TOP表
+   */
+  val MONGO_COLLECTION_GENRE_TOP = "genre_top";
+
+  /**
+   * 基于内容的电影推荐表
+   */
+  val MONGO_COLLECTION_CONTENT = "content_top";
+
+  /**
+   * 基于ALS的用户推荐表
+   */
+  val MONGO_COLLECTION_ALS_USER_RECS = "als_user_recs";
+
+  /**
+   * 基于ALS的电影相似度列表
+   */
+  val MONGO_COLLECTION_ALS_MOVIE_SIM = "als_movie_sim";
+
+  /**
+   * 基于ALS的电影相似度列表
+   */
+  val MONGO_COLLECTION_ITEM_CF_MOVIE_RECS = "item_cf_movie_recs";
+
+
+  /**
+   * MySQL配置
+   */
+  val MYSQL_URL = "jdbc:mysql://192.168.10.181:3306/movie_recommendation?serverTimezone=UTC&useUnicode=true&characterEncoding=utf-8&useSSL=false";
+  val MYSQL_DRIVER = "com.mysql.cj.jdbc.Driver";
+  val MYSQL_USER = "root";
+  val MYSQL_PWD = "Pcy90321.";
+
+  /**
+   * 电影详情表
+   */
+  val MYSQL_TABLE_MOVIE_DETAIL = "movie_detail";
+
+  /**
+   * 电影详情表
+   */
+  val MYSQL_TABLE_MOVIE_TAG = "movie_tag";
+
+  /**
+   * 用户评分表
+   */
+  val MYSQL_TABLE_MOVIE_USER_RATINGS = "movie_user_ratings";
+
+
+}
